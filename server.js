f54820eb005ffeaf740adc52cc21ca53eb8e7816
Xzanx <81507055+Xzanx@users.noreply.github.com>
2021-12-27 14:36:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+let userCount = 0;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
+io.on('connection', socket => {
+  socket.on('join', param => {
+    userCount++
+    io.emit('userCount', userCount)
+  })
+  socket.on('message', param => {
+    io.emit('message', param)
+  })
+  socket.on('disconnect', param => {
+    userCount--
+    io.emit('userCount', userCount)
+  })
+})
 
 http.listen(port, () => {
-  console.log(`Website Ready`);
+  console.log('[WEBSITE] Ready');
 });
